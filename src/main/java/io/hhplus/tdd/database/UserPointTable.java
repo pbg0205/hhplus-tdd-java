df31149cd636cecc46f8d029ae8c6101b1e20293
cooper <pbg0205@naver.com>
2024-12-15 00:09:08 +0900

    refactor: UserPointRepository interface 추가

--- src/main/java/io/hhplus/tdd/database/UserPointTable.java
+++ src/main/java/io/hhplus/tdd/database/UserPointTable.java
@@ -12,15 +12,17 @@ import io.hhplus.tdd.point.UserPoint;
  * 해당 Table 클래스는 변경하지 않고 공개된 API 만을 사용해 데이터를 제어합니다.
  */
 @Component
-public class UserPointTable {
+public class UserPointTable implements UserPointRepository {
 
     private final Map<Long, UserPoint> table = new HashMap<>();
 
+    @Override
     public UserPoint selectById(Long id) {
         throttle(200);
         return table.getOrDefault(id, UserPoint.empty(id));
     }
 
+    @Override
     public UserPoint insertOrUpdate(long id, long amount) {
         throttle(300);
         UserPoint userPoint = new UserPoint(id, amount, System.currentTimeMillis());
