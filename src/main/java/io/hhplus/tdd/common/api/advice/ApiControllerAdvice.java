ab0f5377ea48af67e46055d963d966a50343b75a
cooper <pbg0205@naver.com>
2024-12-15 21:13:37 +0900

    refactor: 에러 핸들링 클래스 common.api.support 하위 패키지 이동
    - 해당 클래스: ErrorResponse, ApiControllerAdvice

--- src/main/java/io/hhplus/tdd/common/api/advice/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/common/api/advice/ApiControllerAdvice.java
@@ -1,9 +1,11 @@
-package io.hhplus.tdd;
+package io.hhplus.tdd.common.api.advice;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import io.hhplus.tdd.common.api.support.error.ErrorResponse;
 
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
