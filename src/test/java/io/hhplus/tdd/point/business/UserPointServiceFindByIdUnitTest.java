097257b0027c3524e0c11ffc70bb9a5e273dec04
cooper <pbg0205@naver.com>
2024-12-16 16:00:17 +0900

    revert: 제거된 포인트 조회 서비스 단위 테스트 코드 원복

--- src/test/java/io/hhplus/tdd/point/business/UserPointServiceFindByIdUnitTest.java
+++ src/test/java/io/hhplus/tdd/point/business/UserPointServiceFindByIdUnitTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import io.hhplus.tdd.point.business.dto.UserPointSelectDTO;
-import io.hhplus.tdd.point.exception.InvalidChargingPointException;
 import io.hhplus.tdd.point.exception.InvalidUserIdException;
 import io.hhplus.tdd.point.exception.UserNotFoundException;
 import io.hhplus.tdd.point.infrastructure.database.UserPoint;
@@ -37,96 +38,52 @@ class UserPointServiceFindByIdUnitTest {
 		this.userPointService = new UserPointServiceImpl(userPointRepository, userRepository);
 	}
 
-	@DisplayName("[실패] 충전 포인트가 음수라면 예외를 반환한다")
-	@Test
-	void failChargingPointBecauseOfChargingPointNegative() {
-		// given
-		final long userId = 1L;
-		final long chargingPoint = -1_000L;
-
-		// when, then
-		assertThatThrownBy(() -> userPointService.charge(userId, chargingPoint))
-			.isInstanceOf(InvalidChargingPointException.class);
+	@DisplayName("[실패] 사용자 식별자가 음수인 경우, 예외를 반환한다")
+	@ParameterizedTest
+	@ValueSource(longs = {-1L, -100L, -500_000_000_00L})
+	void failSelectingUserPointBecauseOfUserIdNegative(long userId) {
+		// given, when, then
+		assertThatThrownBy(() -> userPointService.findById(userId)).isInstanceOf(InvalidUserIdException.class);
 	}
 
-	@DisplayName("[실패] 충전 포인트가 0이라면 예외를 반환한다")
+	@DisplayName("[실패] 사용자 식별자는 0인 경우, 예외를 반환한다")
 	@Test
-	void failChargingPointBecauseOfChargingPointZero() {
+	void failSelectingUserPointBecauseOfUserIdZero() {
 		// given
-		final long userId = 1L;
-		final long chargingPoint = 0L;
+		long userId = 0L;
 
 		// when, then
-		assertThatThrownBy(() -> userPointService.charge(userId, chargingPoint))
-			.isInstanceOf(InvalidChargingPointException.class);
+		assertThatThrownBy(() -> userPointService.findById(userId)).isInstanceOf(InvalidUserIdException.class);
 	}
 
-	@DisplayName("[실패] 충전 포인트가 10만점을 넘으면 예외를 반환한다")
+	@DisplayName("[실패] 미등록 사용자 식별자인 경우, 예외를 반환한다")
 	@Test
-	void failChargingPointBecauseOfMaxChargingPoint() {
+	void failSelectingUserPointBecauseOfUnRegisteredUser() {
 		// given
-		final long userId = 1L;
-		final long chargingPoint = 100_001L;
+		long unRegisteredUserId = 1L;
+
+		when(userRepository.exists(anyLong())).thenReturn(false);
 
 		// when, then
-		assertThatThrownBy(() -> userPointService.charge(userId, chargingPoint))
-			.isInstanceOf(InvalidChargingPointException.class);
-	}
-
-	@DisplayName("[실패] 사용자 식별자가 음수라면 예외를 반환한다")
-	@Test
-	void failChargingPointBecauseOfUserIdNegative() {
-		// given
-		final long userId = -1L;
-		final long chargingPoint = 1L;
-
-		// when, then
-		assertThatThrownBy(() -> userPointService.charge(userId, chargingPoint))
-			.isInstanceOf(InvalidUserIdException.class);
-	}
-
-	@DisplayName("[실패] 사용자 식별자가 음수라면 예외를 반환한다")
-	@Test
-	void failChargingPointBecauseOfUserIdZero() {
-		// given
-		final long userId = 0L;
-		final long chargingPoint = 1L;
-
-		// when, then
-		assertThatThrownBy(() -> userPointService.charge(userId, chargingPoint))
-			.isInstanceOf(InvalidUserIdException.class);
-	}
-
-	@DisplayName("[실패] 미등록 사용자 식별자인 경우 예외를 반환한다")
-	@Test
-	void failChargingPointBecauseOfInvalidUserId() {
-		// given
-		final long userId = 1L;
-		final long chargingPoint = 1L;
-
-		when(userRepository.exists(userId)).thenReturn(false);
-
-		// when, then
-		assertThatThrownBy(() -> userPointService.charge(userId, chargingPoint))
+		assertThatThrownBy(() -> userPointService.findById(unRegisteredUserId))
 			.isInstanceOf(UserNotFoundException.class);
 	}
 
-	@DisplayName("[성공] 특정 사용자의 포인트가 정상 충전된다.")
+	@DisplayName("[성공] 양수이며 등록된 사용자 식별자인 경우, 현재 회원 포인트를 반환한다")
 	@Test
-	void successChargingPoint() {
+	void successSelectingUserPoint() {
 		// given
-		final long userId = 1L;
-		final long chargingPoint = 1_000L;
-		final long currentPoint = 1_000L;
+		long registeredUserId = 1L;
+		long point = 1_000L;
 
-		when(userRepository.exists(userId)).thenReturn(true);
-		when(userPointRepository.selectById(userId))
-			.thenReturn(new UserPoint(userId, currentPoint, System.currentTimeMillis()));
-		when(userPointRepository.insertOrUpdate(anyLong(), anyLong()))
-			.thenReturn(new UserPoint(userId, chargingPoint, System.currentTimeMillis()));
+		when(userRepository.exists(anyLong())).thenReturn(true);
+		when(userPointRepository.selectById(anyLong()))
+			.thenReturn(new UserPoint(registeredUserId, point, System.currentTimeMillis()));
 
-		// when, then
-		assertThat(userPointService.charge(userId, chargingPoint))
-			.isEqualTo(new UserPointSelectDTO(userId, chargingPoint));
+		// when
+		final UserPointSelectDTO userPointSelectDTO = userPointService.findById(registeredUserId);
+
+		// then
+		assertThat(userPointSelectDTO).isEqualTo(new UserPointSelectDTO(registeredUserId, point));
 	}
 }
