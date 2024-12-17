5e9d717437b6f89d6697a507cbeb1c906d761686
cooper <pbg0205@naver.com>
2024-12-17 19:57:51 +0900

    feat: PointHistoryRepository 추가

--- src/main/java/io/hhplus/tdd/point/infrastructure/database/PointHistoryTable.java
+++ src/main/java/io/hhplus/tdd/point/infrastructure/database/PointHistoryTable.java
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Component;
  * 해당 Table 클래스는 변경하지 않고 공개된 API 만을 사용해 데이터를 제어합니다.
  */
 @Component
-public class PointHistoryTable {
+public class PointHistoryTable implements PointHistoryRepository {
     private final List<PointHistory> table = new ArrayList<>();
     private long cursor = 1;
 
+    @Override
     public PointHistory insert(long userId, long amount, TransactionType type, long updateMillis) {
         throttle(300L);
         PointHistory pointHistory = new PointHistory(cursor++, userId, amount, type, updateMillis);
@@ -21,6 +22,7 @@ public class PointHistoryTable {
         return pointHistory;
     }
 
+    @Override
     public List<PointHistory> selectAllByUserId(long userId) {
         return table.stream().filter(pointHistory -> pointHistory.userId() == userId).toList();
     }
